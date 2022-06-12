ee86c1c0e5c3acf396f8eb2e76b12cf3d055d268
CaioCaminha <caminhacaiopro@gmail.com>
2022-06-12 10:37:12 -0300

    Sending processed ids to sqs

--- handler.py
+++ handler.py
@@ -1,5 +1,7 @@
 import datetime
+import json
 import logging
+from urllib import response
 import boto3
 import requests
 from botocore.exceptions import UnknownKeyError
@@ -8,12 +10,17 @@ from bs4 import BeautifulSoup
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
+AWS_REGION = 'us-east-1'
 indeed_base_url = 'https://www.indeed.com/jobs?q&l=Remote&sc=0kf%3Aattr(DSQF7)jt(' \
                          'contract)%3B&rbl=Remote&jlid=aaa2b906602aa8f5&fromage=1'
 indeed_url_job_page = 'https://www.indeed.com/viewjob'
 table_name = 'jf-job-finder'
+
+sqs_client = boto3.client('sqs', region_name=AWS_REGION)
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table(table_name)
+
+
 
 def job_finder(event, context):
 
@@ -23,6 +30,7 @@ def job_finder(event, context):
     soup = BeautifulSoup(html_text, 'lxml')
     
     job_list = soup.find('ul', class_='jobsearch-ResultsList')
+    processed_jobs = []
 
 
     for job_element in job_list:
@@ -42,6 +50,8 @@ def job_finder(event, context):
                 title = div_title.a.span.text
 
                 job_id = div_title.a['id'].replace('job_', '')
+                processed_jobs.append(job_id)
+
                 url = f'{indeed_url_job_page}?jk={job_id}'
 
                 html_job_page = requests.get(url).text
@@ -54,14 +64,19 @@ def job_finder(event, context):
                 job["JobUrl"] = url
                 job["Title"] = title
                 job["Company"] = company.text
-
-                redis.set('id', job_id, ex=86400)
-
-                redis.get('id')
                 
             response_dynamodb = table.put_item(TableName=table_name, Item=job)
             logger.info(f'Job element saved into dynamodb -> {response_dynamodb}')
         except Exception as err:
             logger.error(f'Error occurred {err}') 
-
- 
+    
+    message = {'ids': processed_jobs}
+    response = sqs_client.send_message(
+        QueueUrl="https://sqs.us-east-1.amazonaws.com/926265474128/jf-messages.fifo",
+        MessageBody=json.dumps(message),
+        MessageGroupId='messageGroup1'
+    )
+    if response['ResponseMetadata']['HTTPStatusCode'] is true:
+        logger.info(f'Message sent to sqs | response: {response}')
+    else:
+        logger.error('Could not sent message to sqs')
